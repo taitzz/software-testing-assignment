5c436d82a823025edfe4b1023afb0512a4f78115
taitzz <taivu1602@gmail.com>
2026-01-18 10:36:05 +0700

    init project

---
+++ src/main/java/com/example/unittest/UnitTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.unittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestApplication.class, args);
+	}
+}
